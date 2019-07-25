58f3eea367ae99aba1f725674925e309c913bad0
Laurent Salou <laurent.salou@hotmail.fr>
2019-07-25 23:28:56 +0800

    Verify proof shows the steps till the root

--- src/app/components/proof-path/proof-path.component.ts
+++ src/app/components/proof-path/proof-path.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SHA256 } from 'crypto-js';
 
 @Component({
   selector: 'proveit-proof-path',
@@ -9,11 +10,47 @@ export class ProofPathComponent implements OnInit {
 
   @Input() path: Array<any>;
   @Input() root: string;
+  @Input() hash: string;
+  steps: Array<any>;
 
   constructor(
   ) { }
 
   ngOnInit() {
+    this.steps = this.formatPath(this.path, this.hash, 'full');
+  }
+
+  formatPath(path, hash, treeformat) {
+    const steps = [{
+      op: 'sha256',
+      params: (treeformat === 'full') ? [hash] : undefined,
+      res: hash
+    }];
+    let tmp = steps[0].res;
+    path.forEach(step => {
+      if (step.left === tmp) {
+        steps.push({
+          op: 'append',
+          params: (treeformat === 'full') ? [step.right] : undefined,
+          res: tmp + step.right
+        });
+      } else if (step.right === tmp) {
+        steps.push({
+          op: 'prepend',
+          params: (treeformat === 'full') ? [step.left] : undefined,
+          res: step.left + tmp
+        });
+      } else {
+        throw Error('Cant format tree path');
+      }
+      tmp = steps[steps.length - 1].res;
+      steps.push({
+        op: 'sha256',
+        params: (treeformat === 'full') ? [steps[steps.length - 1].res] : undefined,
+        res: tmp
+      });
+    });
+    return steps;
   }
 
 }
--- src/app/components/verify-proof/verify-proof.component.ts
+++ src/app/components/verify-proof/verify-proof.component.ts
@@ -12,6 +12,7 @@ export class VerifyProofComponent implements OnInit {
 
   finished = false;
   proofs: any = {};
+  hash: string;
 
   constructor(
     private proveIt: ProveitService,
@@ -26,9 +27,9 @@ export class VerifyProofComponent implements OnInit {
     let reader = new FileReader();
     reader.onload = async (event: {target}) => {
       const data = event.target.result;
-      const hash = SHA256(data);
+      this.hash = SHA256(data) + '';
       try {
-        const response = await this.proveIt.verify(hash).toPromise();
+        const response = await this.proveIt.verify(this.hash).toPromise();
         this.proofs = JSON.parse(JSON.stringify(response)).proofs;
         this.snackBar.open('File proof received', 'Success', { duration: 5000 });
         this.finished = true;
@@ -37,6 +38,10 @@ export class VerifyProofComponent implements OnInit {
       }
     };
     reader.readAsBinaryString(file);
+  }
+
+  reset() {
+    this.finished = false;
   }
 
 }
