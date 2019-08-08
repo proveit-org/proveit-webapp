56e2886eafac5c5c957e25ac550fb257b487c271
Laurent Salou <laurent.salou@hotmail.fr>
2019-08-08 16:06:43 +0800

    Show info using animation

--- src/app/components/feature/feature.component.ts
+++ src/app/components/feature/feature.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ElementRef } from '@angular/core';
 import { trigger, state, query, group, style, animate, transition } from '@angular/animations';
 
 @Component({
@@ -6,18 +6,17 @@ import { trigger, state, query, group, style, animate, transition } from '@angul
   templateUrl: './feature.component.html',
   styleUrls: ['./feature.component.scss'],
   animations: [
-    trigger('enterLeave', [
-      transition('void => *', [
-        // 'From' Style
-        style({ opacity: 0.2, transform: 'translateY(-100%)' }),
-        animate('1500ms ease-in')
-      ]),
-      transition('* => void', [
-        animate('1500ms ease-in',
-          // 'To' Style
-          style({ opacity: 1, transform: 'translateY(-100%)' }),
-        )
-      ])
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      state('hide', style({
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      })),
+      transition('show => hide', animate('700ms ease-out')),
+      transition('hide => show', animate('700ms ease-in'))
     ])
   ]
 })
@@ -27,13 +26,30 @@ export class FeatureComponent implements OnInit {
   @Input() title: string;
   @Input() text: string;
 
-  constructor() {
+  state = 'hide';
+
+  constructor(
+    public el: ElementRef,
+  ) {
 
   }
 
   ngOnInit() {
-    console.log(this.icon)
-    console.log(this.title)
+
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop;
+    const topScreenPosition = window.pageYOffset;
+    const screenHeight = window.outerHeight;
+
+    if (componentPosition > topScreenPosition && componentPosition < (topScreenPosition + 0.8 * screenHeight)) {
+      this.state = 'show';
+    } else {
+      this.state = 'hide';
+    }
+
   }
 
 }
