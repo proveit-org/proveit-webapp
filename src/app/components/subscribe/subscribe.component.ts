b2554f2ae2a572784aca0055f3385d0d4b45b3a8
Laurent Salou <laurent.salou@hotmail.fr>
2019-08-09 23:44:27 +0800

    Add signup

--- src/app/components/subscribe/subscribe.component.ts
+++ src/app/components/subscribe/subscribe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ProveitService } from '../../services/proveit.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-subscribe',
@@ -7,13 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscribeComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  loading = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private proveIt: ProveitService,
+    private snackBar: MatSnackBar,
+  ) { }
 
   ngOnInit() {
+
+    this.form = this.formBuilder.group({
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+
   }
 
-  subscribe() {
-
+  async subscribe() {
+    this.loading = true;
+    try {
+      const response = await this.proveIt.signup({language: 'en', ...this.form.value}).toPromise();
+      if (response && response.success) {
+        this.snackBar.open('Successfully signed up!', 'Success', { duration: 5000 });
+      } else {
+        this.snackBar.open('Unable to signup', 'Error', { duration: 5000 });
+      }
+      this.loading = false;
+    } catch (error) {
+      this.loading = false;
+      this.snackBar.open('Unable to signup', 'Error', { duration: 5000 });
+      console.log(error);
+    }
   }
 
 }
